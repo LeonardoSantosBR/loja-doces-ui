691cf3c547acffc6e0d55eeeb2cd4884f2605475
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-17 23:37:30 -0300

    feat: construção tela inicial , header feito, footer,cards em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import Header from "./components/header/header";
+
 function App() {
   return (
     <>
-      <h1 className='bg-green-300'>teste</h1>
+      <div className="w-screen h-screen">
+        <Header />
+
+        <div className="w-screen h-[80%] bg-[#E6E6E6] flex items-center">
+          {"/* cards dos doces */"}
+        </div>
+
+        <div className="w-screen h-[10%] bg-white flex items-center justify-between">
+          <p className="text-2xl font-bold font-sans ml-4">TOTAL: 12,40</p>
+
+          <button className="w-[30%] h-12 bg-red-500 rounded mr-6">
+            <p className="text-2xl  text-white">Finalizar</p>
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  const date = new Date();
+  const today = `${date.getDay() < 10 ? "0" + date.getDay() : date.getDay()}`;
+  const month = `${date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1}`;
+  const year = date.getFullYear();
+  const dateFomated = `${today}/${month}/${year}`;
+
+  return (
+    <>
+      <div className="w-screen h-[10%] bg-amber-400 flex items-center">
+        <p className="text-2xl font-bold font-sans ml-4">
+          LOJA - {dateFomated}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
