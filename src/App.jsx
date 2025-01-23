dbe867bc6e6bc9b043d6773e1e4d8de5e22e0203
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-23 11:25:11 -0300

    feat: ajustes css e inicio da construção do modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 import Header from "./components/header/header";
 import Candies from "./components/candies/candies";
+import NewCandy from "./modals/new-candy";
 
 function App() {
   const [candies, setCandies] = useState([]);
+  const [newCandyIsOpen, setNewCandyIsOpen] = useState(false);
+
   return (
     <>
       <div className="w-screen h-screen">
         <Header />
-
         <div className="w-screen h-[80%] bg-[#E6E6E6]">
           <div className=" w-screen h-[90%] p-2">
-            <button className="w-[30%] h-12 bg-green-500 rounded mr-6">
-              <p className="text-2xl  text-white">Adicionar</p>
+            <button
+              className="
+             w-32
+             h-12
+           bg-green-500 
+             rounded mr-6"
+             onClick={() => setNewCandyIsOpen(!newCandyIsOpen)}
+            >
+              <p className="text-xl  text-white">novo doce</p>
             </button>
-
             <div className="w-[100%] h-[100%] overflow-auto mt-3">
               <Candies candies={candies} setCandies={setCandies} />
             </div>
           </div>
         </div>
-
         <div className="w-screen h-[10%] bg-white flex items-center justify-between">
           <p className="text-2xl font-bold font-sans ml-4">TOTAL: 12,40</p>
           <button className="w-[30%] h-12 bg-red-500 rounded mr-6">
             <p className="text-2xl  text-white">Finalizar</p>
           </button>
         </div>
+        {newCandyIsOpen && <NewCandy />}
       </div>
     </>
   );
---
+++ src/modals/new-candy.jsx
@@ -0,0 +1,17 @@
+import { X } from "lucide-react";
+
+function NewCandy() {
+  return (
+    <>
+      <div className="w-screen h-screen top-0 left-0 absolute bg-zinc-600/55 flex justify-center items-center">
+        <div className="w-[70%] h-[40%] bg-amber-400 rounded">
+          <div className="w-[100%] h-[10%] p-1 flex justify-end">
+            <X />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NewCandy;
