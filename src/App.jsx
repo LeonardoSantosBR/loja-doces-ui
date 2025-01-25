42d9b1c1c4ee4476c3886d8ce0fdac9c3fb33678
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-25 00:43:05 -0300

    fix: identação

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,7 @@ function App() {
         <Header />
         <div className="w-screen h-[80%] bg-[#E6E6E6]">
           <div className="w-screen h-[90%] p-2">
-            <button
-              className="w-32 h-12 bg-green-500 rounded mr-6"
-              onClick={() => dispatch(turn())}
-            >
+            <button className="w-32 h-12 bg-green-500 rounded mr-6" onClick={() => dispatch(turn())}>
               <p className="text-xl  text-white">novo doce</p>
             </button>
             <div className="w-[100%] h-[100%] overflow-auto mt-3">
