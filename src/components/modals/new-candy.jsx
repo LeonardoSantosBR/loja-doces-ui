c7c35d9723c675ced870d570d49b868e5f30c156
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-27 22:10:21 -0300

    fix: estilo do modal de novo doce

--- src/components/modals/new-candy.jsx
+++ src/components/modals/new-candy.jsx
@@ -10,8 +10,9 @@ function NewCandy() {
       <div className="w-screen h-screen top-0 left-0 absolute bg-zinc-600/55 flex justify-center items-center">
         <div className="w-[70%] h-[40%] bg-amber-300 rounded">
           <div className="w-[100%] h-[10%] p-1 flex justify-end">
-            <X onClick={()=> dispatch(turn())}/>
+            <X onClick={() => dispatch(turn())} />
           </div>
+          <div className="w-[100%] h-[100%] bg-slate-700 p-2"></div>
         </div>
       </div>
     </>
