0a61121819aeaa7a42604368f5db65d7317d9b75
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-02-03 16:11:22 -0300

    feat: alert de finalizar loja adicionado

--- src/components/buttons/finish-store.jsx
+++ src/components/buttons/finish-store.jsx
@@ -1,13 +1,30 @@
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 import { useDispatch } from "react-redux";
 import { resetStore } from "../../redux/slices/candies.slice";
 
 function FinishStoreButton() {
   const dispatch = useDispatch();
 
+  const finishStore = async () => {
+    Swal.fire({
+      title: "Tem certeza que deseja finalizar a loja?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Finalizar",
+      denyButtonText: `Voltar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Loja finalizada!");
+        dispatch(resetStore());
+      }
+    });
+  };
+
   return (
     <button
       className="w-[30%] h-12 bg-red-500 rounded mr-6"
-      onClick={() => dispatch(resetStore())}
+      onClick={() => finishStore()}
     >
       <p className="text-xl text-white">finalizar</p>
     </button>
