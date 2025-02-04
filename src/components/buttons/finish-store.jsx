5de9ce5b15b8c6d134aab68ccb754ae46e047b8c
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-02-04 18:54:46 -0300

    feat: gerador de pdf jsPDF

--- src/components/buttons/finish-store.jsx
+++ src/components/buttons/finish-store.jsx
@@ -1,12 +1,42 @@
 import Swal from "sweetalert2/dist/sweetalert2.js";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 import "sweetalert2/src/sweetalert2.scss";
 import { useDispatch } from "react-redux";
 import { resetStore } from "../../redux/slices/candies.slice";
+import { CandiesSelector } from "../../selectors/candies-selector";
 
 function FinishStoreButton() {
   const dispatch = useDispatch();
+  const candies = CandiesSelector();
 
-  const finishStore = async () => {
+  const generatePdf = () => {
+    const doc = new jsPDF();
+    const tablesColumn = ["Nome", "Quantidade", "Preço"];
+
+    const tableRows = candies.map((candy) => {
+      const candyValue = Object.values(candy);
+      candyValue.shift();
+      return candyValue;
+    });
+
+    const totalValue = candies
+      ?.reduce((acc, cur) => {
+        const value = acc + cur.quantidade * Number(cur.preço.split("R$ ")[1]);
+        return value;
+      }, 0)
+      ?.toFixed(2);
+
+    doc.text(`Relatório do dia: R$: ${totalValue}`, 14, 10);
+    doc.autoTable({
+      head: [tablesColumn],
+      body: tableRows,
+      startY: 20,
+    });
+    doc.save("relatorio_vendas.pdf");
+  };
+
+  const finishStore = () => {
     Swal.fire({
       title: "Tem certeza que deseja finalizar a loja?",
       showDenyButton: true,
@@ -15,6 +45,7 @@ function FinishStoreButton() {
       denyButtonText: `Voltar`,
     }).then((result) => {
       if (result.isConfirmed) {
+        generatePdf();
         Swal.fire("Loja finalizada!");
         dispatch(resetStore());
       }
