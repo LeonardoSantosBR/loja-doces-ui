08a02298f8ffeec69ef4adc3fc7933d483372f75
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-02-04 23:27:38 -0300

    fix: gerador de pdf modularizado

---
+++ src/functions/generate-pdf.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import { CandiesSelector } from "../selectors/candies-selector";
+
+export function GeneratorPdf() {
+  const candies = CandiesSelector();
+
+  const generatePdf = () => {
+    const doc = new jsPDF();
+    const tablesColumn = ["Nome", "Quantidade", "Preço"];
+    const tableRows = candies.map(({ id, ...rest }) => Object.values(rest));
+    const totalValue = candies
+      ?.reduce((acc, cur) => {
+        const value = acc + cur.quantidade * Number(cur.preço.split("R$ ")[1]);
+        return value;
+      }, 0)
+      ?.toFixed(2);
+
+    doc.text(`Relatório do dia: R$: ${totalValue}`, 14, 10);
+    doc.autoTable({
+      head: [tablesColumn],
+      body: tableRows,
+      startY: 20,
+    });
+    doc.save("relatorio_vendas.pdf");
+  };
+
+  return generatePdf;
+}
